9d832a817864e9532d7181506c48b28625c07dbf
Ahydul <ahydul1@gmail.com>
2023-11-30 16:07:35 +0100

    Refactor

--- delete_files.py
+++ delete_files.py
@@ -1,26 +1,42 @@
-import os
+import os, shutil
 from os import listdir
 from os.path import isfile, join
 
-path = '/home/ahydul/.var/app/com.atlauncher.ATLauncher/data/instances/Minecraft1201withFabric/mods'
-# onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+# Tk().withdraw() 
+# mods_path = askopenfilename() 
+mods_path = '/home/ahydul/.var/app/com.atlauncher.ATLauncher/data/instances/Minecraft1201withFabric/mods'
 
+file = open("updated_mods.txt", "r")
 
-file = open("update_status.txt", "r")
-updated_mods = False
-for mod in file:
-    if 'Incompatible mods:' in mod:
-        break
+skip_first = True
+for line in file:
+    if skip_first:
+        skip_first = False
+        continue
 
-    if updated_mods:
-        file = path+'/'+mod.replace('\n','')
-        print('Deleting: '+file)
-        if os.path.isfile(file):
-            os.remove(file)
-        else:
-            print("Error: %s file not found" % file)
+    spl = line.split(',')
+    old_mod = spl[0]
+    new_mod = spl[1].replace('\n','')
 
-    if 'Updated mods:' in mod:
-        updated_mods = True
+    delete_file = mods_path+'/'+old_mod
+    copy_file = 'mods/'+new_mod
+
+    if not os.path.isfile(delete_file):
+        print("Error: %s file not found" % delete_file)
+        continue
+    if not os.path.isfile(copy_file):
+        print("Error: %s file not found" % copy_file)
+        continue
+
+    print('Copying: ' + copy_file + ' to '+ mods_path)
+    #src, dst
+    shutil.copy(copy_file, mods_path)
+
+    print('Deleting: ' + delete_file)
+    os.remove(delete_file)
+
+    print('Deleting: ' + copy_file)
+    os.remove(copy_file)
+
 
 file.close()
